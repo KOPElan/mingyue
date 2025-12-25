0613f7e030acd5c368bfc2bd3b2581bccaebd481
kopelan <dongxusheng@live.cn>
2025-12-25 08:44:42 +0800

    新建项目

---
+++ MingYue/Models/SettingModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace MingYue.Models
+{
+    public class SettingModel
+    {
+        public DesignThemeModes Mode { get; set; }
+        public OfficeColor? OfficeColor { get; set; }
+    }
+}
